632487e14754fe401f533797673762b72d8f5f14
coti <costynsando@gmail.com>
2023-10-20 00:40:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("mision").textContent = "Agente que organiza con eficiencia los factores económicos para producir bienes y servicios para el mercado con el ánimo de alcanzar ciertos objetivos"
+document.getElementById("mail").textContent = "contacto@gmail.com"
+document.querySelector('.facebook').textContent = ""
+
+
+
+let titulo = document.getElementById('title')
+let nuevoh2 = document.createElement('h2')
+nuevoh2.textContent = "INFORMACION"
+title.appendChild(nuevoh2)
+
+let nosotros = document.getElementById('us')
+let nuevoh3 = document.createElement('h3')
+nuevoh3.textContent = "ABOUT US"
+title.appendChild(nuevoh3)
